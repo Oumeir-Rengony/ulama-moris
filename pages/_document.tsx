4544e14ab3d58cc13be4e8a10bfce39d5e8936a7
oumeir rengony <renogny.or@pg.com>
2024-03-03 16:42:20 +0400

    fix styles flash on load

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,28 +4,32 @@ import Document, {
   Main,
   NextScript,
   DocumentContext,
-  DocumentInitialProps,
-} from 'next/document'
+} from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
  
 class MyDocument extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext
-  ): Promise<DocumentInitialProps> {
-    const originalRenderPage = ctx.renderPage
- 
-    // Run the React rendering logic synchronously
-    ctx.renderPage = () =>
-      originalRenderPage({
-        // Useful for wrapping the whole react tree
-        enhanceApp: (App) => App,
-        // Useful for wrapping in a per-page basis
-        enhanceComponent: (Component) => Component,
-      })
- 
-    // Run the parent `getInitialProps`, it now includes the custom `renderPage`
-    const initialProps = await Document.getInitialProps(ctx)
- 
-    return initialProps
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+        });
+  
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        )
+      };
+    } finally {
+      sheet.seal();
+    }
   }
  
   render() {
