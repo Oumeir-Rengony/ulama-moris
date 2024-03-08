3d49d2cce5ed700ae24b5c773e226e9d269410c5
oumeir rengony <renogny.or@pg.com>
2024-03-08 17:16:38 +0400

    Added purgecss  remove unused styling

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,31 @@
 module.exports = {
     plugins: {
       tailwindcss: {},
-      autoprefixer: {},
+      cssnano: {},
+      "postcss-flexbugs-fixes": {},
+      "postcss-preset-env": {
+        autoprefixer: {
+          flexbox: "no-2009",
+        },
+        stage: 3,
+        features: {
+          "custom-properties": false,
+        },
+      },
+      '@fullhuman/postcss-purgecss': {
+        content: [
+          './pages/**/*.{js,jsx,ts,tsx}',
+          './components/**/*.{js,jsx,ts,tsx}',
+          "./node_modules/@nextui-org/theme/dist/components/pagination.js", 
+          "./node_modules/@nextui-org/theme/dist/components/input.js", 
+          "./node_modules/@nextui-org/theme/dist/components/button.js", 
+          "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js",
+        ],
+        defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+        safelist: {
+          standard: ["html", "body", /^data-.*/],
+        },
+      }
     },
 }
+
