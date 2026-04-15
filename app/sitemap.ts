4573caa0a7e75fb7f84b5b984e377979b9c5f7ce
Oumeir Rengony <oumeir.me@gmail.com>
2026-04-15 15:24:50 +0400

    added logs

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,6 +6,7 @@ export const dynamic = 'force-dynamic';
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
    const domain = process.env.NEXT_PUBLIC_SITE_URL;
+   console.log(domain)
 
    const audioList = await getBayaanSlug();
 
@@ -15,6 +16,8 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       changeFrequency: 'yearly',
       priority: 0.8,
    }))
+
+   console.log(audioSitemap);
 
    return [
       {
