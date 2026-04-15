381e06366a378863ce99370aaef36a34dc831572
Oumeir Rengony <oumeir.me@gmail.com>
2026-04-15 17:47:23 +0400

    added force dynamic

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,8 @@
 import { getBayaanSlug } from "@/services/bayaans/bayaan.service";
 import { type  MetadataRoute } from "next";
 
+export const dynamic = 'force-dynamic'
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const domain = process.env.NEXT_PUBLIC_SITE_URL;
   
